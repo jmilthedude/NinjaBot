ddfebe716455fc6dad257afe4db9726836b29dc3
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2021-07-12 09:07:28 -0500

    Initial commit: base bot with log

---
+++ src/main/java/net/thedudemc/ninjabot/NinjaBot.java
@@ -0,0 +1,42 @@
+package net.thedudemc.ninjabot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.security.auth.login.LoginException;
+import java.util.EnumSet;
+
+public class NinjaBot extends ListenerAdapter {
+
+    private static Logger LOG = LoggerFactory.getLogger(NinjaBot.class);
+    public static JDA JDA;
+
+    public static void main(String[] args) throws LoginException {
+        getLogger().info("Starting up...");
+
+        if (args.length < 1) {
+            getLogger().error("A token must be provided as the first Program Argument.");
+            return;
+        }
+
+
+        String token = args[0];
+
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.enableIntents(EnumSet.allOf(GatewayIntent.class));
+        builder.addEventListeners(new BotSetup());
+        JDA = builder.build();
+    }
+
+    public static Logger getLogger() {
+        return LOG;
+    }
+
+    public static JDA getJDA() {
+        return JDA;
+    }
+}
